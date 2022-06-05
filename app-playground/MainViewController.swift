27bd2077f809e9a5291d8a911bbfa6fe89742459
uetyo <uehara.yuki@dely.jp>
2022-06-05 14:33:34 +0900

    init project

---
+++ app-playground/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  app-playground
+//
+//  Created by uehara yuki on 2022/05/19.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
